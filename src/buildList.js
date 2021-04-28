e3548a6edd3d6f07b7559cf892538eee1fa68f85
PentagonSwap <pentagonswap@gmail.com>
2021-04-28 20:58:41 +0700

    first commit

---
+++ src/buildList.js
@@ -0,0 +1,41 @@
+const { version } = require('../package.json');
+const mainnet = require('./tokens/mainnet.json');
+const testnet = require('./tokens/testnet.json');
+// const ropsten = require('./tokens/ropsten.json');
+// const rinkeby = require('./tokens/rinkeby.json');
+// const goerli = require('./tokens/goerli.json');
+// const kovan = require('./tokens/kovan.json');
+
+module.exports = function buildList() {
+  const parsed = version.split('.');
+  return {
+    'name': 'PentagonSwap Default List',
+    'timestamp': (new Date().toISOString()),
+    'version': {
+      'major': +parsed[ 0 ],
+      'minor': +parsed[ 1 ],
+      'patch': +parsed[ 2 ]
+    },
+    'tags': {},
+    'logoURI': '/images/coins/WHT',
+    'keywords': [
+      'pentagonswap',
+      'default'
+    ],
+    tokens: [
+      ...mainnet,
+      // ...testnet
+      // ...ropsten,
+      // ...goerli,
+      // ...kovan,
+      // ...rinkeby
+    ]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      })
+  };
+};
